6eb77361dc0a33d0ecd64b15935edf376576644b
PurpleUltraViolet <purple.ultraviolet616@gmail.com>
2020-07-06 09:10:35 -0700

    Add command-line argument

--- src/main.c
+++ src/main.c
@@ -1,15 +1,27 @@
 #include <stdio.h>
-#include "product.h"
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
 #include "inventory.h"
 #include "ui.h"
 
 int main(int argc, char *argv[])
 {
     struct Inventory *inv;
+    int rval;
 
-    inv = inventory_create("Unsaved Inventory");
-    if (!inv)
-        return 1;
+    if (argc >= 2) {
+        if (inventory_deserialize(&inv, argv[1])) {
+            fprintf(stderr, "Error opening file %s: %s\nUsage: %s [file]\n",
+                    argv[1], strerror(errno), argv[0]);
+            return errno;
+        }
+    } else {
+        inv = inventory_create("Unsaved Inventory");
+        if (!inv)
+            return 1;
+    }
 
-    return run(inv);
+    rval = run(inv);
+    return rval;
 }
